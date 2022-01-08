e34d70a3b3f2f647206fcc6704282cc53f35c165
Samuel Gagnon <gagnons.infynox@gmail.com>
2022-01-08 16:40:13 -0500

    Add type parameter.
    Preparing for upcoming fixed values support.

--- Go.ps1
+++ Go.ps1
@@ -7,16 +7,20 @@ while ($true) {
 
     $SplittedDNS = @()
 
-    foreach ($Record in $State.records) {
+    foreach ($Record in $State.records.keys) {
         $SplittedDNS += Split-DNS -DNS $Record
     }
 
     foreach ($Domain in $($SplittedDNS | Select-Object Domain | Get-Unique).Domain) {
         $CloudFlareDNSRecords = Get-CloudFlareZoneDNSRecord -ZoneID $(Get-CloudFlareZone | Where-Object { $_.name -eq $Domain }).id
-        foreach ($CloudFlareDNSRecord in $($CloudFlareDNSRecords | Where-Object { $($State.records) -contains $_.name })) {
+        foreach ($CloudFlareDNSRecord in $($CloudFlareDNSRecords | Where-Object { $($State.records.keys) -contains $_.name })) {
             Write-Host -NoNewline -ForegroundColor Yellow "$(Get-Date -Format s) - Updating $($CloudFlareDNSRecord.name) ... "
+            if ($CloudFlareDNSRecord.type -ne $($State.records.$($CloudFlareDNSRecord.name).type)){
+                Write-Host -NoNewline -ForegroundColor Gray "$(Get-Date -Format s) - Skipping because the type is not matching."
+                continue
+            }
             if ($CloudFlareDNSRecord.content -ne $CurrentIP) {
-                Update-CloudFlareZoneDNSRecord -ZoneID $CloudFlareDNSRecord.zone_id -RecordID $CloudFlareDNSRecord.id -Content $CurrentIP | Out-Null
+                Update-CloudFlareZoneDNSRecord -Type $($State.records.$($CloudFlareDNSRecord.name).type) -ZoneID $CloudFlareDNSRecord.zone_id -RecordID $CloudFlareDNSRecord.id -Content $CurrentIP | Out-Null
                 Write-Host -NoNewline -ForegroundColor Green "`r$(Get-Date -Format s) - Updating $($CloudFlareDNSRecord.name) ... Updated !"
             }
             else { Write-Host -NoNewline -ForegroundColor Gray "`r$(Get-Date -Format s) - Updating $($CloudFlareDNSRecord.name) ... No update needed !" }
